7a2cc51c3b650c03962055a7da982865e51e949d
Farhan Sahibzada <farhansahabzada3@gmail.com>
2025-01-25 03:07:03 +0500

    backend temperorly completed

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ dotenv.config({
 ConnectDB()
 .then(()=> {
     app.listen(process.env.PORT || 8000 , ()=> {
-        console.log(`server is running at port :`, process.env.port || 8000)
+        console.log(`server is running at port :`, process.env.PORT || 8000)
     })
 })
 .catch((error)=> {
